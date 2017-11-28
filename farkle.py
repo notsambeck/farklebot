a65d581de4ced2fbbe292d64e06aa5f18693df3e
Sam Beck <notsambeck@gmail.com>
2017-11-27 16:04:06 -0800

    implemented gradient descent, but it does not work very well. i believe the problem is that the loss function (change in score) does not have a useful gradient - it is zero for the vast majority of examples. need to add some kind of per-example sigmoid loss function; the problem is how to do this without an explicit correct answer for each dice roll.

--- farkle.py
+++ farkle.py
@@ -17,7 +17,7 @@ class Farkle:
     def __init__(self, verbosity=0):
         self.current_score = 0
         # self.theta = [20.0, -1, -.05, .05, 20]
-        self.theta = np.random.rand(5)
+        self.theta = np.random.rand(3)
         self.score_history = []
         self.v = verbosity
 
@@ -80,36 +80,22 @@ class Farkle:
                 return 2500, 0
             # single three-of-a-kind
             else:
-                # greedy
+                # score three of a kind
                 three_of = c.pop(c.most_common(1)[0][0])
                 if three_of == 1:
                     temp_score = 300
                 else:
                     temp_score = three_of * 100
                 used = 3
-                if greedy or list(c.keys()) in ([1, 5], [5, 1]):
+
+                # score remainder
+                if greedy or list(c.keys()) in ([1, 5], [5, 1]):   # use all if possible or if greedy
                     temp_score += c[1] * 100
                     used += c[1]
                     temp_score += c[5] * 50
                     used += c[5]
                     return temp_score, n - used
-                else:
-                    '''
-                    # ultra-passive option
-                    if c[1]:
-                        return 100, n - 1
-                    elif c[5]:
-                        return 50, n - 1
-
-                    else:
-                        three_of = c.pop(c.most_common(1)[0][0])
-                        return three_of * 100, n - 3
-                    '''
-                    three_of = c.pop(c.most_common(1)[0][0])
-                    if three_of == 1:
-                        temp_score = 300
-                    else:
-                        temp_score = three_of * 100
+                else:   # use only 3 of a kind otherwise
                     return temp_score, n - 3
 
         # 3 pairs
@@ -172,10 +158,10 @@ class Farkle:
         # return True to continue or False to stop.
         score = self.current_score
         t = self.theta
-        activation = sum([num_dice * t[0],
-                          np.log10(score + .000001) * t[1],
-                          score * t[2],
-                          num_dice / (score + 1) * self.theta[3]])
+        activation = sum([num_dice/6 * t[0],
+                          score/1000 * t[1]])
+        # np.log10(score + .000001) * t[1],
+        # num_dice / (score + 1) * self.theta[3]])
         return activation > t[-1]
 
     def display(self):
@@ -199,55 +185,79 @@ if __name__ == '__main__':
     best_score = 0
     best_theta = f.theta.copy()
 
-    games = 10000
+    games = 60000
     f.score_history = []
 
     # baseline
     for game in range(games):
         f.play()
-    f.display()
+    # f.display()
 
     last = np.mean(f.score_history)
     best_time = 0
-    ratio = 1e-4
+    delta = 1
 
-    print(f.theta)
+    print('initial score = {}, theta={}'.format(last, f.theta))
 
     # change parameters, if it works keep it
-    for step in range(100):
+    for step in range(1000):
+        if last == 0:
+            last = 1
 
-        if step - best_time > 5:
-            ratio *= 10
+        # gradient - score(theta + delta) - score(theta)
+        gradient = np.zeros(len(f.theta))
+
+        if step - best_time > 15:
+            delta /= 3
             best_time = step
-            print('ratio =', ratio)
-            if ratio > 10:
+            print('delta =', delta)
+            if delta < .01:
+                # end when changes are done
                 f.theta = best_theta.copy()
                 break
 
+        # compute partials (by brute force)
         for w in range(len(f.theta)):
-            amt = (random.random() - .5) / ratio
+            # save old weight
             temp = f.theta[w]
-            f.theta[w] *= 1 + amt
 
+            # try increasing theta[w]
+            f.theta[w] += delta
             f.score_history = []
             for _ in range(games):
                 f.play()
+            plus = np.mean(f.score_history) / last
 
-            current = np.mean(f.score_history)
-            print('adjusting {} to {:.2}; best={}, current={}'.format(w, f.theta[w], best_score, current))
+            # try decreasing
+            f.theta[w] = temp - delta
+            f.score_history = []
+            for _ in range(games):
+                f.play()
+            minus = np.mean(f.score_history) / last
 
-            if current >= last:
-                last = current
-            else:
-                f.theta[w] = temp
+            gradient[w] = (plus - minus)
 
-            # print('best score =', best_score, 'current score =', current)
+            # reset theta
+            f.theta[w] = temp
 
-            if current > best_score:
-                print('new best:', f.theta)
-                best_time = step
-                best_score = current
-                best_theta = f.theta.copy()
+        f.theta += gradient * delta
+        print('for step=', step)
+        print('gradient  =', gradient)
+        print('theta set =', f.theta)
+
+        # test new position
+        f.score_history = []
+        for _ in range(games):
+            f.play()
+
+        current = np.mean(f.score_history)
+        print('new score =', current)
+
+        if current > best_score:
+            best_time = step
+            best_theta = f.theta.copy()
+            best_score = current
+        last = current
 
     f.theta = best_theta.copy()
 
